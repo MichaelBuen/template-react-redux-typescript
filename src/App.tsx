c0c4dc9eb9ca50896a58380307658326527218d5
dev <ienablemuch@gmail.com>
2019-03-17 14:24:16 +0800

    hot-reloading

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,53 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+import { hot } from 'react-hot-loader/root';
+
+
+interface IComponentState
+{
+    message: string;
+    counter: number;
 }
 
-export default App;
+class App extends Component<object, IComponentState>
+{
+    constructor(props: {})
+    {
+        super(props);
+
+        this.state = {
+            message: 'hello world',
+            counter: 0,
+        };
+    }
+
+    increment = () => this.setState({counter: this.state.counter + 1});
+
+    render()
+    {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p>
+                        Edit <code>src/App.tsx</code> and save to reload.
+                    </p>
+
+                    <p>{this.state.message}</p>
+                    <button onClick={this.increment}>Increment {this.state.counter}</button>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                </header>
+            </div>
+        );
+    }
+}
+
+export default hot(App);
